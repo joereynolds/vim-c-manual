5f72d5619ea65e0bac432cbbdcdb633a45fb4019
Joe Reynolds <joereynolds952@gmail.com>
2017-07-02 17:49:12 +0100

    SHE LIVES

--- private/main.py
+++ private/main.py
@@ -7,7 +7,23 @@ import re
 
 def get_chapters():
     """Returns all chapters from the C reference manual"""
-    pass
+    manual = get_file_contents('../manual/creference')
+
+    chapter_header_pattern= r'^\d+.+ The \w+ function$'
+    chapter_headers = re.findall(chapter_header_pattern, manual, re.M)
+
+    chapters = []
+    for i, header in enumerate(chapter_headers):
+
+        # Crappy hack around index errors
+        try:
+            chapter_contents = manual[manual.index(header): manual.index(chapter_headers[i+1])]
+            chapters.append(chapter_contents)
+            write_chapter_to_file(header, chapter_contents)
+        except IndexError:
+            continue
+
+    return chapters
 
 def get_file_contents(file_path):
     """Return the file contents as a string"""
@@ -15,14 +31,14 @@ def get_file_contents(file_path):
     file_contents = [line for line in file_to_read]
     return ''.join(file_contents)
 
-def main():
+def write_chapter_to_file(chapter_title, chapter_contents):
     manual = get_file_contents('../manual/creference')
+    chapter = open('../manual/docs/' + chapter_title, 'w')
+    chapter.write(chapter_contents)
+    chapter.close()
 
-    function_pattern = r'the \w+ function .*'
-    result = re.findall(function_pattern, manual, re.M)
-    print(result)
-    print(manual)
-
+def main():
+    get_chapters()
 
 main()
 
